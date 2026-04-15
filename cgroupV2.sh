d16b68fa8a80acb3305b51c2b0e712f7f7d0495f
Elie <niyubwayoiraelie5777@gmail.com>
2026-04-15 23:56:34 +0200

    add comment to clarify the purpose of writing values to the cgroup knobs in cgroupV2.sh

--- cgroupV2.sh
+++ cgroupV2.sh
@@ -4,6 +4,7 @@ sudo bash -c 'echo "+cpu +memory +pids" > /sys/fs/cgroup/cgroup.subtree_control'
 # create a cgroup
 sudo mkdir -p /sys/fs/cgroup/nsbox
 
+# write values to the knobs of the cgroup
 sudo bash -c 'echo "256M" > /sys/fs/cgroup/nsbox/memory.max'
 sudo bash -c 'echo "128M" > /sys/fs/cgroup/nsbox/memory.high'
 sudo bash -c 'echo "500000 1000000" > /sys/fs/cgroup/nsbox/cpu.max'
